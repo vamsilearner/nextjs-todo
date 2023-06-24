db1ec9b676ba6e0118d05d7a4367d09c536e5266
KANCHARLA VAMSIKRISHNA PRASAD <65493037+vamsilearner@users.noreply.github.com>
2023-06-24 21:33:12 +0530

    nextjs todo app

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { prisma } from "./db";
+import TodoItem from "@/components/TodoItem";
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
+
+
+export default async function Home() {
+  const todos = await getTodos();
+  // await prisma.todo.create({ data: {title: "test", complete: false}})
+
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1>To Dos list</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+
+      <ul className="pl-4">
+        {todos.map((todo: any) => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
+    </>
+  );
+}
